cb43e04b221ce3df3f6573d334ad3654d90bb3fe
krish036 <90232766+krish036@users.noreply.github.com>
2022-01-15 23:15:36 +0530

    Create pratical2_1_e.py

---
+++ pratical2_1_e.py
@@ -0,0 +1,9 @@
+# Write a Python script to concatenate the following dictionaries to create a new one.
+#d21cs109 krish pandya
+dict1={1:20,2:20}
+dict2={3:30,4:40}
+dict3={5:50,6:60}
+temp={}
+for val in (dict1,dict2,dict3):
+    temp.update(val)
+print(temp)
