47c81c2e18622427b1b3ea63ba319af164aa1f94
krish036 <90232766+krish036@users.noreply.github.com>
2022-01-15 23:26:01 +0530

    Create pratical2_3_a.py

---
+++ pratical2_3_a.py
@@ -0,0 +1,8 @@
+# Write a Python program to add member(s) in a set and clear a set
+#d21cs109 krish pandya
+odd_num={1,3,5,7}
+print(odd_num)
+odd_num.add(9)
+print(odd_num)
+odd_num.clear()
+print(odd_num)
