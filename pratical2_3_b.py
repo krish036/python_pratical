bbabcdce9643471676de903d82e62390cf0db32d
krish036 <90232766+krish036@users.noreply.github.com>
2022-01-15 23:26:41 +0530

    Create pratical2_3_b.py

---
+++ pratical2_3_b.py
@@ -0,0 +1,6 @@
+# Write a Python program to remove an item from a set if it is present in the set.
+#d21cs109 krish pandya
+set={'Krish','Navsari','CSPIT','Changa'}
+print(set)
+set.remove('Navsari')
+print(set)
