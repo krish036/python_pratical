645292e7863cc0100e04b63b30ae91533d43f6d8
krish036 <90232766+krish036@users.noreply.github.com>
2022-01-15 23:27:19 +0530

    Create pratical2_3_c.py

---
+++ pratical2_3_c.py
@@ -0,0 +1,7 @@
+# Write a Python program to create an intersection, Union, difference of sets.
+#d21cs109 krish pandya
+Odd = {0, 2, 4, 6, 8}
+Even = {1, 2, 5, 7, 9}
+print("Intersection: ", Odd & Even)  #intersection
+print("Union: ", Odd | Even)  #union
+print("Difference: ", Odd-Even)  #difference
