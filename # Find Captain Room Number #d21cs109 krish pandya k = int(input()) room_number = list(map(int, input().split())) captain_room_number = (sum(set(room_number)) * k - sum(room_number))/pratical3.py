e53d6ba3a0e7d532e0687571af1068234d769bdc
krish036 <90232766+krish036@users.noreply.github.com>
2022-02-04 17:57:42 +0530

    Create pratical3.py

---
+++ # Find Captain Room Number #d21cs109 krish pandya k = int(input()) room_number = list(map(int, input().split())) captain_room_number = (sum(set(room_number)) * k - sum(room_number))/pratical3.py
@@ -0,0 +1,6 @@
+# Find Captain Room Number
+#d21cs109 krish pandya
+k = int(input())
+room_number = list(map(int, input().split()))
+captain_room_number = (sum(set(room_number)) * k - sum(room_number))  // (k - 1)
+print(captain_room_number)
