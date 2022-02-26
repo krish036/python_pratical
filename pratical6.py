03480e18e259c1a911a71a3b578ef6a1c1c4605b
krish036 <90232766+krish036@users.noreply.github.com>
2022-02-26 22:29:05 +0530

    Create pratical6.py

---
+++ pratical6.py
@@ -0,0 +1,11 @@
+#krish d21cs109
+from collections import Counter
+l=[]
+for i in range(int(input())):
+    l.append(input())
+
+c=Counter(l)
+print(len(c))
+for i in c:
+    print(c.get(i),end=' ')
+print()
